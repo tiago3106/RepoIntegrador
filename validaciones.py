33d48c51683d57371daf41f9f93f43084388484a
tiago3106 <tiagogd3101@gmail.com>
2025-10-27 09:33:39 -0300

    Update validaciones.py

--- validaciones.py
+++ validaciones.py
@@ -6,6 +6,14 @@ def validar_numero(caracteres):
     except ValueError:
         condicion = False
     return condicion
+def verificar_existencia_archivo(nombre_archivo):
+    try:
+        with open(nombre_archivo, 'r', encoding='utf-8') as archivo:
+            # Si el archivo se abre correctamente, salimos del bucle
+            bucle = False
+            return bucle
+    except FileNotFoundError:
+        print("El archivo no existe. Por favor, ingrese un nombre de archivo válido.")
 
 #agregar que validaciones haran falta
 #formas de traer una funcion de otro archivo
